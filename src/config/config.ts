303f1e161b12abe48516ccc5907651189ebfdc59
Henri DF <henridf@gmail.com>
2021-06-22 16:30:24 +0200

    Add mechanism to bypass rate limiter

--- src/config/config.ts
+++ src/config/config.ts
@@ -4,4 +4,5 @@ export const config = {
     web3_url: process.env.WEB3_PROVIDER_URI,
     redis_url: process.env.REDIS_URL || 'redis://localhost:6379',
     redis_key_prefix: process.env.NODE_ENV === 'test' ? 'test:' : '',
+    rate_bypass: process.env.RATE_BYPASS_TOKEN,
 };
